1e888a5adc2cbf43d7db47d8964d58129af3fc80
pradumn <pradumnprasad883@gmail.com>
2022-01-01 00:55:15 +0530

    Adding implementation of Hashing using Quadratic Probing.

---
+++ Hashing/Quadratic-Probing/Quadratic-probing.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#define SIZE 10        
+using namespace std;
+int hashval(int key)    // function to give the index at which the key is to be stored in the hash table.
+{
+    return key%SIZE;
+}
+int quadratic_probe(int H[],int key)  // function to give the index to the first vacant space according to the quadratic probing hash function h`(x).
+{
+    int index=hashval(key);
+    int i=0;
+    while(H[(index+i*i)%SIZE]!=0)
+    {
+        i++;
+    }
+    return (index+i*i)%SIZE;
+}
+void insert(int H[],int key)  // function to store a given key in the given hash table.
+{
+   int index=hashval(key);
+   if(H[index]==0)
+   {
+       H[index]=key;
+   }
+   else
+   {
+     index=quadratic_probe(H,key);
+     H[index]=key;
+   }
+}
+int Search(int H[],int key)  // function to return the index of the given key in the given hash table.
+{
+    int index=hashval(key);
+    int i=0;
+    while(H[(index+i*i)%SIZE]!=key&&H[(index+i*i)%SIZE]!=0)
+    {
+        i++;
+    }
+    if(H[(index+i*i)%SIZE]==0)
+    {
+       return -1;
+    }
+    return (index+i*i)%SIZE;
+}
+int main()
+{
+    int HT[SIZE]={0};   // Creating hash table.
+    // Inserting keys in the hash table.
+    insert(HT,23);
+    insert(HT,43);
+    insert(HT,13);
+    insert(HT,27);
+    insert(HT,37);
+    // Printing the hash table.
+    for(int i=0;i<SIZE;i++)
+    {
+        cout<<i<<" : "<<HT[i]<<endl;
+    }
+    // Searching a key in the hash table.
+    int key=12;
+    if(Search(HT,key)==-1)
+    {
+        cout<<"key is not present in the hash table."<<endl;
+    }
+    else
+    {
+        cout<<"key is found at index "<<Search(HT,key)<<endl;
+    }
+    return 0;
+}
