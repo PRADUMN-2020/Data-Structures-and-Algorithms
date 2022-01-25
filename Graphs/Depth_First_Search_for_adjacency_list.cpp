ae8195308308e6d81326f119f85f792367d61795
pradumn <pradumnprasad883@gmail.com>
2022-01-25 22:34:05 +0530

    Adding DFS Space Complexity.

--- Graphs/Depth_First_Search_for_adjacency_list.cpp
+++ Graphs/Depth_First_Search_for_adjacency_list.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // int visited[8]={0};
 void DFS(vector<int>al[],int u,int n)           // Function to print dfs traversal of the given graph in the form of an adjacency list.
 {                                               // Time:O(n+e) n: no of vertices e: no of edges. 
-   static int visited[8]={0};
+   static int visited[8]={0};                   // Space:O(n)
    if(visited[u]==0)
    {
        cout<<u<<" ";
