7409a64884eb5a15c9d825208a407e98a6b30644
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-09-27 15:17:04 +0530

    Update Floyd_warshall_Algorithm.cpp
    Simplified the Floyd Warshall Algorithm.

--- Graphs/Floyd_warshall_Algorithm.cpp
+++ Graphs/Floyd_warshall_Algorithm.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define inf 100000
+#define inf 1e9
 void floyd(int graph[][5],int n)   // Time:O(n^3) where n is the no of vertices of the given.
 {
   for(int k=1;k<=n;k++)
@@ -9,7 +9,7 @@ void floyd(int graph[][5],int n)   // Time:O(n^3) where n is the no of vertices 
       {
           for(int j=1;j<=n;j++)
           {   
-              if(i!=j&&i!=k&&j!=k)
+              // if(i!=j&&i!=k&&j!=k) [not imp]
               graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j]);
           }
       }
@@ -23,7 +23,7 @@ int main()
                      {0,5,inf,0,1},
                      {0,2,inf,inf,0},
                        };
-    floyd(graph,4);          // After calling floyd's algorithm the adjacency matrix will be updated with the min cost to reach from i to j at index i, j.
+    floyd(graph,4);          // After calling floyd's algorithm the cost adjacency matrix will be updated with the min cost to reach from i to j at index i, j.
     for(int i=1;i<5;i++)
     {
         for(int j=1;j<5;j++)
